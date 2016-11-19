8c803646c30d49366c56f405ca72597f9a582184
Boris <iamborisbutenko@gmail.com>
2016-11-19 19:27:54 +0300

    1600

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -1,8 +1,89 @@
 'use strict';
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
 (function ($) {
 
     $(function () {
+
+        /**
+         * Left menu click && Page scroller
+         */
+        $('nav.menu-left, .map').on('click', '[data-scroll]', function (event) {
+
+            event.preventDefault();
+
+            var target = $(this).attr('href'),
+                top = $(target).offset().top,
+                func = $(this).data('scroll');
+
+            if (target == '#main__test' || target == '#main__article') top -= 50;
+            if (target == '#main__timeline') top += 50;
+
+            $('body,html').stop(true).animate({
+                scrollTop: top
+            }, 1500, function () {
+                if (func != '') eval(func);
+            });
+
+            return false;
+        });
+
+        var Scroller = function () {
+            function Scroller(value) {
+                _classCallCheck(this, Scroller);
+
+                this.count = value;
+            }
+
+            _createClass(Scroller, [{
+                key: 'updateScrollOptions',
+                value: function updateScrollOptions(value, className) {
+                    if (~className.indexOf('timeline__img')) return false;
+
+                    var position = $(window).scrollTop();
+
+                    if (value > 0 && this.count >= value * 3 || value < 0 && this.count <= value * 3) {
+
+                        if (position >= 0 && value > 0) $('a[href*=main__article]').trigger('click');
+
+                        if (position >= 950 && value < 0) $('a[href*=main__head]').trigger('click');
+
+                        if (position >= 950 && value > 0) $('a[href*=main__test]').trigger('click');
+
+                        if (position >= 1480 && value < 0) $('a[href*=main__article]').trigger('click');
+
+                        if (position >= 1480 && value > 0) $('a[href*=main__timeline]').trigger('click');
+
+                        if (position >= 1990 && value < 0) $('a[href*=main__test]').trigger('click');
+
+                        this.count = 0;
+
+                        return false;
+                    }
+
+                    this.count += value;
+                }
+            }]);
+
+            return Scroller;
+        }();
+
+        function scroll(top, func) {
+            if (func) $('body,html').animate({
+                scrollTop: top
+            }, 1500, func);else $('body,html').animate({
+                scrollTop: top
+            }, 1500);
+        }
+
+        var scroller = new Scroller(0);
+
+        window.onwheel = function (e) {
+            scroller.updateScrollOptions(e.deltaY, e.target.className);
+        };
 
         /**
          * Tooltips
@@ -30,27 +111,14 @@
          */
         $('.timeline .timeline__item').hover(function () {
             $(this).find('img').toggleClass('timeline__hover').end().find('span').toggleClass('timeline__text-hover');
+
+            return false;
         });
 
         /**
-         * Left menu click
+         * Parallax scrool
          */
-        $('nav.menu-left, .map').on('click', '[data-scroll]', function (event) {
-
-            event.preventDefault();
-
-            var target = $(this).attr('href'),
-                top = $(target).offset().top,
-                func = $(this).data('scroll');
-
-            if (target == '#main__test') top -= 50;
-
-            $('body,html').animate({
-                scrollTop: top
-            }, 1500, function () {
-                eval(func);
-            });
-        });
+        parallaxScroll();
 
         window.onscroll = function () {
             parallaxScroll();
@@ -59,57 +127,51 @@
         function parallaxScroll() {
             var scrolled = $(window).scrollTop();
 
-            $('.parallax__left-item1').css('top', 0 + scrolled * .25 + 'px');
+            $('.parallax__left-item1').css('top', 0 - scrolled * .15 + 'px');
 
-            $('.parallax__left-item2').css('top', 0 + scrolled * .15 + 'px');
+            $('.parallax__left-item2').css('top', 0 - scrolled * .50 + 'px');
 
             if (scrolled > 600) {
                 $('.parallax__left-item3').css('top', 0 - scrolled * .45 + 'px');
                 $('.parallax__left-item4').css('top', 0 - scrolled * .65 + 'px');
             }
+
+            $('.parallax__right-item1').css('top', 0 - scrolled * .35 + 'px');
+
+            $('.parallax__right-item2').css('top', 0 - scrolled * .80 + 'px');
+
+            $('.parallax__right-item3').css('top', 0 - scrolled * .30 + 'px');
+
+            $('.parallax__right-item4').css('top', 0 - scrolled * .5 + 'px');
+
+            $('.parallax__right-item5').css('top', scrolled * .28 + 'px');
+
+            return false;
         }
 
         /**
          * Image animation => section main__test
          */
         function animateSectionTest() {
-            if ($(window).scrollTop() > 1450) {
-                $('.main__test-image2').animate({
-                    opacity: 1,
-                    left: '-1.8rem'
-                }, 800, function () {
-                    $('.main__test-image1, .main__test-image3').animate({
-                        opacity: 1
-                    }, 300);
-                });
+            $('.main__test-image2').animate({
+                opacity: 1,
+                left: '-1.8rem'
+            }, 800, function () {
+                $('.main__test-image1, .main__test-image3').animate({
+                    opacity: 1
+                }, 300);
+            });
 
-                $('.main__test-image5').animate({
-                    opacity: 1,
-                    right: 0
-                }, 800, function () {
-                    $('.main__test-image4').animate({
-                        opacity: 1
-                    }, 300);
-                });
-            } else if ($(window).scrollTop() > 2200 || $(window).scrollTop() < 930) {
-                $('.main__test-image2').animate({
-                    opacity: 1,
-                    left: '-52.5rem'
-                }, 800, function () {
-                    $('.main__test-image1, .main__test-image3').animate({
-                        opacity: 0
-                    }, 300);
-                });
+            $('.main__test-image5').animate({
+                opacity: 1,
+                right: 0
+            }, 800, function () {
+                $('.main__test-image4').animate({
+                    opacity: 1
+                }, 300);
+            });
 
-                $('.main__test-image5').animate({
-                    opacity: 0,
-                    right: '-38rem'
-                }, 800, function () {
-                    $('.main__test-image4').animate({
-                        opacity: 0
-                    }, 300);
-                });
-            }
+            return false;
         }
 
         /**
@@ -125,6 +187,8 @@
                 opacity: 1,
                 right: '2.5rem'
             }, 800);
+
+            return false;
         }
     });
 })(jQuery);
--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -5,6 +5,90 @@
     $(() => {
 
         /**
+         * Left menu click && Page scroller
+         */
+        $('nav.menu-left, .map').on('click', '[data-scroll]', function (event) {
+
+            event.preventDefault();
+
+            let target = $(this).attr('href'),
+                top = $(target).offset().top,
+                func = $(this).data('scroll');
+
+            if (target == '#main__test' || target == '#main__article') top -= 50;
+            if (target == '#main__timeline') top += 50;
+
+            $('body,html')
+                .stop(true)
+                .animate({
+                    scrollTop: top
+                }, 1500, function () {
+                    if ( func != '' ) eval(func);
+                });
+
+            return false;
+        });
+
+        class Scroller {
+            constructor(value) {
+                this.count = value;
+            }
+
+            updateScrollOptions(value, className) {
+                if ( ~className.indexOf('timeline__img') ) return false;
+
+                let position = $(window).scrollTop();
+
+                if (value > 0 && this.count >= value * 3
+                    || value < 0 && this.count <= value * 3) {
+
+                    if ( position >= 0 && value > 0 )
+                        $('a[href*=main__article]').trigger('click');
+
+                    if ( position >= 950 && value < 0 )
+                        $('a[href*=main__head]').trigger('click');
+
+                    if ( position >= 950 && value > 0)
+                        $('a[href*=main__test]').trigger('click');
+
+                    if ( position >= 1480 && value < 0 )
+                        $('a[href*=main__article]').trigger('click');
+
+                    if ( position >= 1480 && value > 0 )
+                        $('a[href*=main__timeline]').trigger('click');
+
+                    if ( position >= 1990 && value < 0 )
+                        $('a[href*=main__test]').trigger('click');
+
+                    this.count = 0;
+
+                    return false;
+                }
+
+                this.count += value;
+            }
+        }
+
+        function scroll(top, func) {
+            if ( func )
+                $('body,html')
+                    .animate({
+                        scrollTop: top
+                    }, 1500, func);
+            else
+                $('body,html')
+                    .animate({
+                        scrollTop: top
+                    }, 1500);
+        }
+
+        let scroller = new Scroller(0);
+
+        window.onwheel = function (e) {
+            scroller.updateScrollOptions(e.deltaY, e.target.className)
+        };
+
+        /**
          * Tooltips
          */
         $('[data-tooltip]').tooltip({
@@ -34,41 +118,28 @@
                 .find('img').toggleClass('timeline__hover')
                 .end()
                 .find('span').toggleClass('timeline__text-hover');
+
+            return false;
         });
 
         /**
-         * Left menu click
+         * Parallax scrool
          */
-        $('nav.menu-left, .map').on('click', '[data-scroll]', function(event) {
-
-            event.preventDefault();
-
-            let target = $(this).attr('href'),
-                top = $(target).offset().top,
-                func = $(this).data('scroll');
-
-            if ( target == '#main__test' ) top -= 50;
-
-            $('body,html').animate({
-                scrollTop: top
-            }, 1500, function() {
-                eval(func);
-            });
-        });
+        parallaxScroll();
 
         window.onscroll = function () {
             parallaxScroll();
         };
 
-        function parallaxScroll(){
+        function parallaxScroll() {
             let scrolled = $(window).scrollTop();
 
             $('.parallax__left-item1').css(
-                'top', (0 + (scrolled*.25)) + 'px'
+                'top', (0 - (scrolled*.15)) + 'px'
             );
 
             $('.parallax__left-item2').css(
-                'top', (0 + (scrolled*.15)) + 'px'
+                'top', (0 - (scrolled*.50)) + 'px'
             );
 
             if ( scrolled > 600 ) {
@@ -79,49 +150,53 @@
                     'top', (0 - (scrolled * .65)) + 'px'
                 );
             }
+
+            $('.parallax__right-item1').css(
+                'top', (0 - (scrolled*.35)) + 'px'
+            );
+
+            $('.parallax__right-item2').css(
+                'top', (0 - (scrolled*.80)) + 'px'
+            );
+
+            $('.parallax__right-item3').css(
+                'top', (0 - (scrolled * .30)) + 'px'
+            );
+
+            $('.parallax__right-item4').css(
+                'top', (0 - (scrolled * .5)) + 'px'
+            );
+
+            $('.parallax__right-item5').css(
+                'top', ((scrolled * .28)) + 'px'
+            );
+
+            return false;
         }
 
         /**
          * Image animation => section main__test
          */
         function animateSectionTest() {
-            if ($(window).scrollTop() > 1450) {
-                $('.main__test-image2').animate({
-                    opacity: 1,
-                    left: '-1.8rem'
-                }, 800, function () {
-                    $('.main__test-image1, .main__test-image3').animate({
-                        opacity: 1
-                    }, 300)
-                });
+            $('.main__test-image2').animate({
+                opacity: 1,
+                left: '-1.8rem'
+            }, 800, function () {
+                $('.main__test-image1, .main__test-image3').animate({
+                    opacity: 1
+                }, 300)
+            });
 
-                $('.main__test-image5').animate({
-                    opacity: 1,
-                    right: 0
-                }, 800, function () {
-                    $('.main__test-image4').animate({
-                        opacity: 1
-                    }, 300)
-                });
-            } else if ($(window).scrollTop() > 2200 || $(window).scrollTop() < 930) {
-                $('.main__test-image2').animate({
-                    opacity: 1,
-                    left: '-52.5rem'
-                }, 800, function () {
-                    $('.main__test-image1, .main__test-image3').animate({
-                        opacity: 0
-                    }, 300)
-                });
+            $('.main__test-image5').animate({
+                opacity: 1,
+                right: 0
+            }, 800, function () {
+                $('.main__test-image4').animate({
+                    opacity: 1
+                }, 300)
+            });
 
-                $('.main__test-image5').animate({
-                    opacity: 0,
-                    right: '-38rem'
-                }, 800, function () {
-                    $('.main__test-image4').animate({
-                        opacity: 0
-                    }, 300)
-                });
-            }
+            return false;
         }
 
         /**
@@ -137,6 +212,8 @@
                 opacity: 1,
                 right: '2.5rem'
             }, 800);
+
+            return false;
         }
 
     });
